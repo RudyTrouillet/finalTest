f143d076eea71686594bbbd26cc039e1932a07f8
unknown <rudy.trouillet@gmail.com>
2015-06-25 11:26:12 +0200

    Test RécupérerLesNombresPairs & RécupérerLesNombresPairsBis

---
+++ FinalTest.Tests/Nombres.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalTest.Tests
+{
+    public class Nombres
+    {
+        private IEnumerable<KeyValuePair<string, int>> keyValuePairs;
+
+        public Nombres(IEnumerable<KeyValuePair<string, int>> keyValuePairs)
+        {
+            // TODO: Complete member initialization
+            this.keyValuePairs = keyValuePairs;
+        }
+        public IEnumerable<int> NombresPairs
+        {
+            get
+            {
+                return keyValuePairs.Where(x => x.Value % 2 == 0).Select(x => x.Value);
+            }
+        }
+    
+    }
+}
