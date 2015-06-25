3d97e6cfccb52cb3c928a0b7f56ccbc06a9ea169
unknown <rudy.trouillet@gmail.com>
2015-06-25 09:55:53 +0200

    Test DéfinirUneClasseRealisantUneMultiplication

---
+++ FinalTest/Multiplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTest
+{
+   public class Multiplication
+    {
+       public bool PeutCalculer(string calcul)
+       {
+           return (calcul.Contains("*"));
+       }
+       public int Calculer(String calcul)
+       {
+           var splitted = new string[2];
+           splitted=calcul.Split('*');
+           return Convert.ToInt32(splitted[0]) * Convert.ToInt32(splitted[1]);
+       }
+    }
+}
