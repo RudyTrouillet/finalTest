76801f6457f0ad7912fcc23f408fd97e128ddf9a
unknown <rudy.trouillet@gmail.com>
2015-06-25 09:28:46 +0200

    Test DéfinirUnTypeRéférenceAvecEgalitéDeuxInstancesAyantLesMêmesPropriétés

---
+++ FinalTest/TypeReference.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTest
+{
+    public class TypeReference
+    {
+        private int p;
+
+        public TypeReference(int p)
+        {
+            // TODO: Complete member initialization
+            this.p = p;
+
+        }
+        public override bool Equals(object obj)
+        {
+            return((TypeReference)obj).p == this.p;
+        }
+    }
+}
